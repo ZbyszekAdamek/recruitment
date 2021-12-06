fe72f7e573f91a64c1baac938fd4b72dc4e04117
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-12-06 21:13:12 +0100

    Utworzenie klasy studenta i nauczyciela oraz utworzenie walidacji imion oraz wieku

--- src/main/java/com/example/recruitment/entity/Student.java
+++ src/main/java/com/example/recruitment/entity/Student.java
@@ -1,4 +1,109 @@
 package com.example.recruitment.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
 public class Student {
+
+    private String name;
+    private String surname;
+    private Integer age;
+    private String email;
+    private String course;
+    private Long id;
+    @ManyToMany
+    private List<Teacher> teachers;
+
+    public Student(String name, String surname, Integer age, String email, String course, Long id, List<Teacher> teachers) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+        this.course = course;
+        this.id = id;
+        this.teachers = teachers;
+    }
+
+    public Student() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String studentName) {
+        if(studentName.length() < 2){
+            throw new IllegalArgumentException();
+        }
+        this.name = studentName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer ageParameter) {
+        if(ageParameter < 18){
+            throw new IllegalArgumentException();
+        }
+        this.age = ageParameter;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                ", id=" + id +
+                ", teachers=" + teachers +
+                '}';
+    }
 }
--- src/main/java/com/example/recruitment/entity/Teacher.java
+++ src/main/java/com/example/recruitment/entity/Teacher.java
@@ -1,4 +1,109 @@
 package com.example.recruitment.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
 public class Teacher {
+
+    private String name;
+    private String surname;
+    private Integer age;
+    private String email;
+    private String subject;
+    private Long id;
+    @ManyToMany
+    private List<Student> students;
+
+    public Teacher(String name, String surname, Integer age, String email, String subject, Long id, List<Student> students) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+        this.subject = subject;
+        this.id = id;
+        this.students = students;
+    }
+
+    public Teacher() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String teacherName) {
+        if(teacherName.length() < 2){
+            throw new IllegalArgumentException();
+        }
+        this.name = teacherName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer ageParameter) {
+        if(ageParameter < 18){
+            throw new IllegalArgumentException();
+        }
+        this.age = ageParameter;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", subject='" + subject + '\'' +
+                ", id=" + id +
+                ", students=" + students +
+                '}';
+    }
 }
